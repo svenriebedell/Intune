e72940f1392cef7a1b9476e8d0c5cc72598ab1e3
Sven Riebe @ Dell <99394991+svenriebedell@users.noreply.github.com>
2022-03-15 16:56:31 +0100

    Create Intune_4_Compliance_Sensor_WMI_Chassis_Intrusion.ps1

---
+++ CustomCompliance/Intune_4_Compliance_Sensor_WMI_Chassis_Intrusion.ps1
@@ -0,0 +1,47 @@
+﻿<#
+_author_ = Sven Riebe <sven_riebe@Dell.com>
+_twitter_ = @SvenRiebe
+_version_ = 1.0.0
+_Dev_Status_ = Test
+Copyright © 2022 Dell Inc. or its subsidiaries. All Rights Reserved.
+
+No implied support and test in test environment/device before using in any production environment.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+#>
+
+<#Version Changes
+
+1.0.0   inital version
+
+
+#>
+
+<#
+.Synopsis
+   This PowerShell is for custom compliance scans and is checking this device of chassis intrusions.
+   IMPORTANT: This scipt need a client installation of Dell Command Update UWP first. https://www.dell.com/support/kbdoc/en-us/000177325/dell-command-update
+   IMPORTANT: WMI BIOS is supported only on devices which developt after 2018, older devices does not supported by this powershell
+   IMPORTANT: This script does not reboot the system to apply or query system.
+.DESCRIPTION
+   Powershell using WMI to check the BIOS value of chassis intrusion. This script need to be upload in Intune Compliance / Script and need a JSON file additional for reporting this value.
+   
+#>
+
+# check chassis intrusion with WMI
+$CheckChassis = Get-CimInstance -Namespace root/dcim/sysman/biosattributes -ClassName EnumerationAttribute -Filter "AttributeName like 'ChasIntrusion'" | select -ExpandProperty CurrentValue
+
+
+#prepare variable for Intune
+$hash = @{ Intrusion = $CheckChassis }
+
+#convert variable to JSON format
+return $hash | ConvertTo-Json -Compress
