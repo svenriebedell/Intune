2d4ae443304f92cf34783743471d94aa53451bb5
Sven Riebe @ Dell <99394991+svenriebedell@users.noreply.github.com>
2022-03-17 19:29:22 +0100

    Update Intune_9_remediation_DCM_OutofWarranty.ps1

--- Remediation/Intune_9_remediation_DCM_OutofWarranty.ps1
+++ Remediation/Intune_9_remediation_DCM_OutofWarranty.ps1
@@ -27,15 +27,19 @@ limitations under the License.
 
 <#
 .Synopsis
-   This PowerShell is for custom compliance scans and is checking the support contract time of this device by Dell Command Monitor (DCM)
+   This PowerShell is for remediation and is checking the support contract time of this device by Dell Command Monitor (DCM) and User will informed by popup to order a new device. User need to click ok otherwise he will informed again.
    IMPORTANT: This scipt need a client installation of Dell Command Monitor https://www.dell.com/support/kbdoc/en-us/000177080/dell-command-monitor
    IMPORTANT: This script does not reboot the system to apply or query system.
 .DESCRIPTION
-   Powershell using Dell Command Monitor WMI to check the support contract time of the device. This script need to be upload in Intune Compliance / Script and need a JSON file additional for reporting this value.
+   Powershell using Dell Command Monitor WMI to check the support contract time of the device. This Script need to be imported in Reports/Endpoint Analytics/Proactive remediation. This File is for remediation only and need a seperate script for detection additional.
    
 #>
 
 
+# Time popup is closing
+$PopupTime = 20 #time in sec.
+
+<#
 # prepare Dell Warranty date for compare with actual date
 $WarrantyEnd = Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_AssetWarrantyInformation | Sort-Object -Descending | select -ExpandProperty WarrantyEndDate 
 $WarrantyEndSelect = $WarrantyEnd[0] -split ","
@@ -45,11 +49,77 @@ $WarrantyDate = $WarrantyEndSelect -split " "
 # Check availible support days
 $Today = Get-Date
 $Duration = New-TimeSpan -Start $Today -End $FinalDate
-$Over45Days = New-TimeSpan -Start $Today -End $FinalDate.AddDays(45)
+#>
 
-If ($FinalDate -le Get-Date)
+$Duration = 50
+
+#Checking warranty and inform user 45 days before out of warranty and out of warrenty
+
+If ($Duration -le 45)
     {
 
-    [System.Windows.Forms.MessageBox]::Show("This Device is since "$Duration" out of Warranty"$([System.Environment]::NewLine),"Please open a support ticket to order a new device" ,"Device out of Warranty notification",3,[System.Windows.Forms.MessageBoxIcon]::Exclamation)
-    [System.Windows.Forms.MessageBox]::Show("This Device is ",$Duration," out of warrenty",$([System.Environment]::NewLine))
+    #setup a registrykey to control user has read this information
+    New-Item -path "HKLM:\SOFTWARE\Dell\Warranty" -Force
+
+
+    If ($Duration -le 0)
+        {
+        
+        #creating object os WScript
+        $wshell = New-Object -ComObject Wscript.Shell -ErrorAction Stop
+        #invoking the POP method using object
+        $CheckUserFeedback = $wshell.Popup("This Device is since $Duration Days of of Service. Please, you should open a ServiceNow case to get a new device. If you ignore this message your device will deactivate soon.",$PopupTime,"Device Out of Warranty",64)
+
+        If ($CheckUserFeedback -notmatch 1)
+            {
+
+            New-ItemProperty -Path "HKLM:\SOFTWARE\Dell\Warranty" -Name "Info" -Value "no confirmation" -type string -Force
+            Write-Output "Device has no support and user ignored message"
+            exit 1
+
+            }
+        
+        Else
+            {
+
+            New-ItemProperty -Path "HKLM:\SOFTWARE\Dell\Warranty" -Name "Info" -Value "Informed" -type string -Force
+            Write-Output "Device has no support and user is informed"
+            exit 0
+
+            }
+        }
+    Else
+        {
+
+        #creating object os WScript
+        $wshell = New-Object -ComObject Wscript.Shell -ErrorAction Stop
+        #invoking the POP method using object
+        $CheckUserFeedback = $wshell.Popup("This Device will be out of service in $Duration Days. Please, you should open a ServiceNow case to get a new device. If you ignore this message your device will deactivate soon.",$PopupTime,"Device Out of Warranty",64)
+
+        If ($CheckUserFeedback -notmatch 1)
+            {
+
+            New-ItemProperty -Path "HKLM:\SOFTWARE\Dell\Warranty" -Name "Info" -Value "no confirmation" -type string -Force
+            Write-Output "Device has less than 45 days of support and user ignored message"
+            exit 1
+
+            }
+        
+        Else
+            {
+
+            New-ItemProperty -Path "HKLM:\SOFTWARE\Dell\Warranty" -Name "Info" -Value "Informed" -type string -Force
+            Write-Output "Device has less than 45 days of support and user is informed"
+            exit 0
+
+            }
+        }  
+
+    Else
+        {
+
+        Write-Output "Device has more than 45 days of support"
+        exit 0
+                
+        }
     }
