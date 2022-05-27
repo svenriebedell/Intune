98d0c6c862bb46d58f2938269450a25e1c4f3c55
Sven Riebe @ Dell <99394991+svenriebedell@users.noreply.github.com>
2022-05-27 14:46:37 +0200

    Update Intune_1_Detection_BIOS_AdminPW_Setting.ps1

--- Remediation/Intune_1_Detection_BIOS_AdminPW_Setting.ps1
+++ Remediation/Intune_1_Detection_BIOS_AdminPW_Setting.ps1
@@ -31,7 +31,7 @@ limitations under the License.
 try{
    
     # Check BIOS AttributName AdminPW is set
-    $BIOSAdminPW = Get-CimInstance -Namespace root/dcim/sysman/wmisecurity -ClassName PasswordObject -Filter "NameId='Admin'" | select -ExpandProperty IsPasswordSet
+    $BIOSAdminPW = Get-CimInstance -Namespace root/dcim/sysman/wmisecurity -ClassName PasswordObject -Filter "NameId='Admin'" | Select-Object -ExpandProperty IsPasswordSet
 
     if ($BIOSAdminPW -match "1")
         {
